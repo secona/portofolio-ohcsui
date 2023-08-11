a1bfe5b88371a0ab2f1480c9cf5b800656211b9b
secona <77039267+secona@users.noreply.github.com>
2023-08-11 23:23:47 +0700

    feat: TypingCode pause when mouse is hovering over

--- components/TypingCode.tsx
+++ components/TypingCode.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { cnProps } from "@/utils/classnames";
 import { Card, CardProps } from "./Card";
 import { useEffect, useState } from "react";
 
@@ -9,29 +10,36 @@ export interface TypingCodeProps extends CardProps {
 export const TypingCode = ({ code, ...props }: TypingCodeProps) => {
   const [text, setText] = useState("");
   const [index, setIndex] = useState(0);
+  const [running, setRunning] = useState(true);
 
   useEffect(() => {
-    if (index < code.length) {
-      const timeout = setTimeout(() => {
-        setText(text + code[index]);
-        setIndex(index + 1);
-      }, 50);
+    if (running)
+      if (index < code.length) {
+        const timeout = setTimeout(() => {
+          setText(text + code[index]);
+          setIndex(index + 1);
+        }, 50);
 
-      return () => clearTimeout(timeout);
-    } else {
-      const timeout = setTimeout(() => {
-        setText("");
-        setIndex(0);
-      }, 3000);
+        return () => clearTimeout(timeout);
+      } else {
+        const timeout = setTimeout(() => {
+          setText("");
+          setIndex(0);
+        }, 3000);
 
-      return () => clearTimeout(timeout);
-    }
+        return () => clearTimeout(timeout);
+      }
   });
 
   if (text === "") return <></>;
 
   return (
-    <Card titleBar {...props}>
+    <Card
+      onMouseEnter={() => setRunning(false)}
+      onMouseLeave={() => setRunning(true)}
+      titleBar
+      {...cnProps(props, "select-none")}
+    >
       <pre>{text}</pre>
     </Card>
   );
