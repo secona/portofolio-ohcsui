7d1f834db7fbd1faedacb16e3d5ff3b570d76c6a
secona <77039267+secona@users.noreply.github.com>
2023-08-11 10:31:01 +0700

    feat: improve GitHubRepo component
    * add image to GitHubRepo
    
    * prevent stretch in displaying GitHubRepo in page.tsx
    
    * add task-tracker and pinisi screenshots

--- app/page.tsx
+++ app/page.tsx
@@ -49,8 +49,10 @@ export default function Home() {
         </Card>
       </Section>
       <Section className="bg-yellow-300">
-        <div className="flex flex-wrap gap-4">
+        <div className="flex flex-wrap gap-4 items-start">
           <GitHubRepo
+            imageSrc="/task-tracker-screenshot.png"
+            imageAlt="Task Tracker Screenshot"
             path="secona/task-tracker"
             description="Task tracking application"
             langLogos={
@@ -62,8 +64,10 @@ export default function Home() {
             }
           />
           <GitHubRepo
+            imageSrc="/pinisi-screenshot.png"
+            imageAlt="Pinisi Screenshot"
             path="secona/pinisi"
-            description="File explorer in the command line"
+            description="File explorer in the command line (under development)"
             langLogos={
               <>
                 <RustLogo />
--- components/GitHubRepo.tsx
+++ components/GitHubRepo.tsx
@@ -2,16 +2,26 @@ import Link from "next/link";
 import { Card } from "./Card";
 import { ReactNode } from "react";
 import { ExternalLink } from "react-feather";
+import Image from "next/image";
 
 interface GitHubRepoProps {
   path: string;
   description: string;
   langLogos?: ReactNode;
+  imageSrc: string;
+  imageAlt: string;
 }
 
 export const GitHubRepo = (props: GitHubRepoProps) => {
   return (
     <Card className="py-6 px-8 w-96">
+      <Image
+        width={1920}
+        height={1080}
+        src={props.imageSrc}
+        alt={props.imageAlt}
+        className="mb-6"
+      />
       <Link
         href={`https://github.com/${props.path}`}
         className="text-xl font-bold underline text-center"
