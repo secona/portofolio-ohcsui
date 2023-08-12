7fba7eed01c4327636668b55817031a4a1066ba0
secona <77039267+secona@users.noreply.github.com>
2023-08-12 18:26:58 +0700

    feat: add title to GitHubProjects
    * add title of "My Projects"

--- content/GitHubProjects.tsx
+++ content/GitHubProjects.tsx
@@ -8,31 +8,34 @@ import { TSLogo } from "@/icons/TSLogo";
 export const GitHubProjects = () => {
   return (
     <Section id="github-projects" className="bg-yellow-300">
-      <div className="flex flex-wrap gap-4 items-start">
-        <GitHubRepo
-          imageSrc="/task-tracker-screenshot.png"
-          imageAlt="Task Tracker Screenshot"
-          path="secona/task-tracker"
-          description="Task tracking application"
-          langLogos={
-            <>
-              <ReactLogo />
-              <SCSSLogo />
-              <TSLogo />
-            </>
-          }
-        />
-        <GitHubRepo
-          imageSrc="/pinisi-screenshot.png"
-          imageAlt="Pinisi Screenshot"
-          path="secona/pinisi"
-          description="File explorer in the command line (under development)"
-          langLogos={
-            <>
-              <RustLogo />
-            </>
-          }
-        />
+      <div className="flex flex-col gap-9">
+        <h1 className="text-7xl">My Projects</h1>
+        <div className="flex flex-row gap-4">
+          <GitHubRepo
+            imageSrc="/task-tracker-screenshot.png"
+            imageAlt="Task Tracker Screenshot"
+            path="secona/task-tracker"
+            description="Task tracking application"
+            langLogos={
+              <>
+                <ReactLogo />
+                <SCSSLogo />
+                <TSLogo />
+              </>
+            }
+          />
+          <GitHubRepo
+            imageSrc="/pinisi-screenshot.png"
+            imageAlt="Pinisi Screenshot"
+            path="secona/pinisi"
+            description="File explorer in the command line (under development)"
+            langLogos={
+              <>
+                <RustLogo />
+              </>
+            }
+          />
+        </div>
       </div>
     </Section>
   );
