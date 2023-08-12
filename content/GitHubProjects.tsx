6d10e9e56c46286cddcc2b7e2f5f0625dfe00478
secona <77039267+secona@users.noreply.github.com>
2023-08-12 19:36:45 +0700

    feat: implement responsive in GitHubProjects
    * add mineswpr to GitHubProjects

--- content/GitHubProjects.tsx
+++ content/GitHubProjects.tsx
@@ -7,10 +7,10 @@ import { TSLogo } from "@/icons/TSLogo";
 
 export const GitHubProjects = () => {
   return (
-    <Section id="github-projects" className="bg-yellow-300">
+    <Section id="github-projects" className="bg-yellow-300 py-24 px-5">
       <div className="flex flex-col gap-9">
-        <h1 className="text-7xl">My Projects</h1>
-        <div className="flex flex-row gap-4">
+        <h1 className="text-7xl text-center">My Projects</h1>
+        <div className="flex flex-row flex-wrap gap-4 items-start justify-center">
           <GitHubRepo
             imageSrc="/task-tracker-screenshot.png"
             imageAlt="Task Tracker Screenshot"
@@ -29,6 +29,17 @@ export const GitHubProjects = () => {
             imageAlt="Pinisi Screenshot"
             path="secona/pinisi"
             description="File explorer in the command line (under development)"
+            langLogos={
+              <>
+                <RustLogo />
+              </>
+            }
+          />
+          <GitHubRepo
+            imageSrc="/mineswpr-screenshot.png"
+            imageAlt="Mineswpr Screenshot"
+            path="secona/mineswpr"
+            description="Minesweeper clone in the command line written in Rust."
             langLogos={
               <>
                 <RustLogo />
