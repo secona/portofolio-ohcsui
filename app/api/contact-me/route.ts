43f6dcbb75af6d0a650e87f6270ce9d866b5f40e
secona <77039267+secona@users.noreply.github.com>
2023-08-12 13:37:41 +0700

    feat: implement sending email

--- app/api/contact-me/route.ts
+++ app/api/contact-me/route.ts
@@ -1,9 +1,29 @@
 import { NextRequest, NextResponse } from "next/server";
+import nodemailer from "nodemailer";
+
+export const email = nodemailer.createTransport({
+  service: process.env.EMAIL_SERVICE,
+  auth: {
+    user: process.env.EMAIL_ADDRESS,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
 
-  console.log(body);
+  await email.sendMail({
+    from: `Contact Me Form <${process.env.EMAIL_ADDRESS}>`,
+    to: process.env.EMAIL_TO,
+    subject: `[CMF] ${body.subject}`,
+    html: `
+<p>Email: ${body.email}</p>
+<p>Name: ${body.name}</p>
+<p>Subject: ${body.subject}</p>
+<br />
+<p>${body.message}</p>
+    `,
+  });
 
   return NextResponse.json({ message: "success" });
 }
