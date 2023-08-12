9f23f668e9135102fec43dad6b237d5b6e98bce8
secona <77039267+secona@users.noreply.github.com>
2023-08-12 18:45:09 +0700

    feat: implement responsive in AboutMe

--- content/AboutMe.tsx
+++ content/AboutMe.tsx
@@ -2,9 +2,12 @@ import { Section } from "@/components/Section";
 
 export const AboutMe = () => {
   return (
-    <Section id="about-me" className="bg-orange-400 grid grid-cols-2 gap-0">
+    <Section
+      id="about-me"
+      className="bg-orange-400 grid h-[200vh] lg:h-[100vh] grid-rows-2 grid-cols-1 lg:grid-rows-1 lg:grid-cols-2 gap-0"
+    >
       <div className="h-full w-full"></div>
-      <div className="px-36 bg-blue-300 h-full w-full border-l-4 border-solid border-black flex flex-col justify-center">
+      <div className="px-16 sm:px-24 md:px-48 lg:px-24 xl:px-36 bg-blue-300 h-full w-full border-t-4 lg:border-t-0 border-l-0 lg:border-l-4 border-solid border-black flex flex-col justify-center">
         <h1 className="text-7xl mb-4">About Me</h1>
         <p className="text-lg">
           I am a Computer Science student at Universitas Indonesia. I have been
